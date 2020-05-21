ff0bf96073c1785ba05b492bc47fea885c852fb8
Pizzagami <samuhelg@gmail.fr>
2020-05-21 17:43:35 +0200

    alpha semafort

---
+++ philo_two/init_philo.c
@@ -0,0 +1,100 @@
+#include "philo_two.h"
+
+int		ft_atoi(const char *str)
+{
+	int				i;
+	int				ret;
+	unsigned long	result;
+
+	i = 0;
+	ret = 1;
+	if (!str)
+		return (0);
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == ' ')
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+	{
+		if (str[i] == '-')
+			ret = -1;
+		i++;
+	}
+	result = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		result = result * 10 + (str[i] - '0');
+		i++;
+	}
+	return ((int)result * ret);
+}
+
+int		fill_and_error(t_philo *philo, char **argv, int argc)
+{
+	if (argc < 5 || argc >= 7)
+	{ 
+		write(2, "error while parsing argv\n", 25);
+		return (1);
+	}
+	else if (argc == 6)
+		philo->number_of_time_each_philosophers_must_eat = ft_atoi(argv[5]);
+	else
+		philo->number_of_time_each_philosophers_must_eat = -1;
+	philo->number_of_philosopher = ft_atoi(argv[1]);
+	philo->time_to_die = ft_atoi(argv[2]) * 1000;
+	philo->time_to_eat = ft_atoi(argv[3]) * 1000;
+	philo->time_to_sleep = ft_atoi(argv[4]) * 1000;
+	return (0);
+}
+
+char    *ft_itoa_next(unsigned long long dec,
+    unsigned long long nb, int size)
+{
+    int             test;
+    char    *res;
+
+    test = 0;
+    res = malloc(size * sizeof(char));
+    while (test < size)
+    {
+        if (nb >= 10)
+        {
+            dec = dec / 10;
+            res[test] = nb / dec + '0';
+            nb -= nb / dec * dec;
+        }
+        else
+            res[test] = nb % 10 + '0';
+        test++;
+    }
+    res[test] = '\0';
+    return (res);
+}
+
+char    *ft_itoa(unsigned long long nb)
+{
+    unsigned long long      test;
+    unsigned int            size;
+    unsigned long long      dec;
+
+    dec = 1;
+    size = 1;
+    test = nb;
+    while (test / 10 != 0)
+    {
+        size++;
+        test = test / 10;
+    }
+    test = 0;
+    while (test++ < size)
+        dec *= 10;
+    return (ft_itoa_next(dec, nb, size));
+}
+
+int ft_strlen(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
---
+++ philo_two/philo_two.c
@@ -0,0 +1,74 @@
+#include "philo_two.h"
+
+void copy_struct(t_philo *paste, t_philo copy)
+{
+	paste->number_of_philosopher = copy.number_of_philosopher;
+	paste->number_of_time_each_philosophers_must_eat = copy.number_of_time_each_philosophers_must_eat;
+	paste->time_to_die = copy.time_to_die;
+	paste->time_to_eat = copy.time_to_eat;
+	paste->time_to_sleep = copy.time_to_sleep;
+	paste->t_start = copy.t_start;
+}
+
+int create_start_philo(int nbr, t_philo philo)
+{
+	t_philo arr[nbr];
+	pthread_t th[nbr];
+	sem_t mutext;
+	sem_t dead;
+	int j;
+
+	j = 0;
+	sem_init(&mutext, 0, philo.number_of_philosopher);
+	sem_init(&dead, 0, 1);
+	sem_wait(&mort);
+	gettimeofday(&philo.t_start, NULL);
+	philo.dead = 0;
+	while (j < philo.number_of_philosopher)
+	{
+		copy_struct(&arr[j], philo);
+		arr[j].dead = 0;
+		arr[j].number = j;
+
+		arr[j].die = &mort;
+		arr[j].mutext = &mutext;
+		arr[j].mutext2 = &mutext;
+
+		pthread_create(&th[j], NULL, &ft_philosopher, &arr[j]);
+		usleep(5000);
+		++j;
+	}
+
+	pthread_t win;
+	pthread_t lose;
+	philo.th = (pthread_t*)&th;
+	philo.die = &mort;
+		pthread_create(&win, NULL, &winner, &philo);
+	pthread_create(&lose, NULL, &loser, &philo);
+
+	while (1)
+	{
+		if (philo.dead == 1)
+		{
+			printf("End of simulation : one of the philosophers died\n");
+			return (1);
+		}
+		else if (philo.dead == 2)
+		{
+			printf("End of simulation : philosophers ate enough times\n");
+			return (0);
+		}
+		usleep(1000);
+	}
+}
+
+int main(int argc, char **argv)
+{
+	t_philo philo;
+	int j;
+
+	srand(time(0));
+	if (fill_and_error(&philo, argv, argc) == 1)
+		return (1);
+	return (create_start_philo(philo.number_of_philosopher, philo));
+}
