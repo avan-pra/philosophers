3f791e715fc7cf6ff9641f5acdbf2239683dae1c
Pizzagami <57038272+Pizzagami@users.noreply.github.com>
2020-05-22 14:56:22 +0200

    parsing safety

--- philo_two/init_philo.c
+++ philo_two/init_philo.c
@@ -27,22 +27,40 @@ int		ft_atoi(const char *str)
 	return ((int)result * ret);
 }
 
-int		fill_and_error(t_philo *philo, char **argv, int argc)
+int        fill_and_error(t_philo *philo, char **argv, int argc)
 {
-	if (argc < 5 || argc >= 7)
-	{ 
-		write(2, "error while parsing argv\n", 25);
-		return (1);
-	}
-	else if (argc == 6)
-		philo->number_of_time_each_philosophers_must_eat = ft_atoi(argv[5]);
-	else
-		philo->number_of_time_each_philosophers_must_eat = -1;
-	philo->number_of_philosopher = ft_atoi(argv[1]);
-	philo->time_to_die = ft_atoi(argv[2]) * 1000;
-	philo->time_to_eat = ft_atoi(argv[3]) * 1000;
-	philo->time_to_sleep = ft_atoi(argv[4]) * 1000;
-	return (0);
+    if (argc < 5  argc >= 7)
+    { 
+        write(2, "Error while parsing argv\n", 25);
+        return (1);
+    }
+    else
+        philo->number_of_time_each_philosophers_must_eat = (argc == 6) ? ft_atoi(argv[5]) : -1;
+    philo->number_of_philosopher = ft_atoi(argv[1]);
+    philo->time_to_die = ft_atoi(argv[2]) * 1000;
+    philo->time_to_eat = ft_atoi(argv[3]) * 1000;
+    philo->time_to_sleep = ft_atoi(argv[4]) * 1000;
+    if (philo->time_to_die < 50000  philo->time_to_die > 1000000)
+    { 
+        write(2, "Error : value out of range for time to die\n", 25);
+        return (1);
+    }
+    if (philo->time_to_sleep < 0  philo->time_to_sleep > 1000000)
+    { 
+        write(2, "Error : value out of range for time to sleep\n", 25);
+        return (1);
+    }
+    if (philo->time_to_eat < 0  philo->time_to_eat > 1000000)
+    { 
+        write(2, "Error : value out of range for time to eat\n", 25);
+        return (1);
+    }
+    if (philo->number_of_philosopher < 0 || philo->number_of_philosopher > 10000)
+    { 
+        write(2, "Error : value out of range for number of philosophers\n", 25);
+        return (1);
+    }
+    return (0);
 }
 
 char    *ft_itoa_next(unsigned long long dec,
